e499675c425ab42bbc75495df7168630e0552d20
Lucas Freitas <devlucao@gmail.com>
2026-01-04 15:45:36 -0300

    feat: Adiciona resolução dos exercícios de 8 a 14.

--- dia03.js
+++ dia03.js
@@ -84,3 +84,81 @@ Considere o array abaixo:
 Calcule a soma total dos valores.*/
 const scores = [10, 8, 9, 7, 6];
 
+const sumScores = scores.reduce((acc, score) => acc + score, 0);
+
+console.log(sumScores);
+
+// como ficaria o exercício 8 usando o return?
+
+/* Exercício 9 — reduce (objeto acumulador)
+Considere o array abaixo:
+Gere um objeto contendo o total vendido por produto.
+*/
+const sales = [
+  { product: "Notebook", value: 3000 },
+  { product: "Mouse", value: 100 },
+  { product: "Notebook", value: 3000 },
+  { product: "Mouse", value: 100 }
+  ]; 
+
+  const totalSales = sales.reduce((acc, productValue) => acc + productValue.value, 0);
+  const obj = { total: totalSales };
+
+  console.log(obj);
+
+/*
+Exercício 10 — sort
+Considere o array abaixo:
+Gere um novo array ordenado em ordem crescente sem modificar o array original.
+*/
+const ages = [33, 21, 45, 18, 29];
+
+const orderedAges = ages.sort();
+
+console.log(orderedAges);
+
+/*Exercício 11 — slice
+Considere o array abaixo:
+Gere um novo array contendo apenas os três primeiros elementos. */
+const items = ["a", "b", "c", "d", "e"];
+
+const slicedItems = items.slice(0, 3);
+
+console.log(slicedItems);
+
+/* Exercício 12 — concat
+Considere os arrays abaixo:
+Gere um novo array contendo todos os membros dos dois times.*/
+
+const teamA = ["Lucas", "Ana"];
+const teamB = ["Pedro", "Marina"];
+
+const teamsConcat = teamA.concat(...teamB);
+
+console.log(teamsConcat);
+
+/* Exercício 13 — flat
+Considere o array abaixo:
+Gere um novo array contendo todos os números em um único nível. */
+const matrix = [[1, 2], [3, 4], [5, 6]];
+
+const arr = matrix.flat(1)
+
+console.log(arr);
+
+/* Exercício 14 — flatMap
+Considere o array abaixo:
+Gere um array contendo todas as habilidades. */
+
+const usrs = [
+  { name: "Lucas", skills: ["JS", "Node"] },
+  { name: "Ana", skills: ["HTML", "CSS"] },
+  { name: "Pedro", skills: ["JS", "React"] }
+];
+
+const allSkills = usrs.flatMap(usrs => usrs.skills)
+
+console.log(allSkills)
+
+// Então o flatmap recebe como parâmetro uma callback function onde tem como argumento o nome do array a ser utilizado e depois da "arrow" o dado do objeto a ser inserido no novo array, é isso?
+
